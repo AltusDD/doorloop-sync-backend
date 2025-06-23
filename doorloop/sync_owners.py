105e31220c61987d0756463ee455ef0b15a9e017
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-06-23 14:25:56 -0500

    final flat

--- doorloop/sync_owners.py
+++ doorloop/sync_owners.py
@@ -1,7 +1,10 @@
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import requests
 from dotenv import load_dotenv
-from ..altus_supabase.client import upsert_records
+from altus_supabase.client import upsert_records
 
 load_dotenv()
 
