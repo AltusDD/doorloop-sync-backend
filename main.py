caa504704d204ee37ad862a79ea0f35a71533886
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-06-23 13:54:50 -0500

    main update2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append("C:/Users/Dionr/OneDrive/Documents/GitHub/doorloop-sync-backend")
 
 from fastapi import FastAPI
 from doorloop import (
