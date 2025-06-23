a644bf025539e61eed4c7fac8daafc9a6707c320
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-06-23 14:47:36 -0500

    flattupdate hardpatch

--- doorloop/sync_vendors.py
+++ doorloop/sync_vendors.py
@@ -1,7 +1,7 @@
-import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append("C:/Users/Dionr/OneDrive/Documents/GitHub/doorloop_sync_backend")
 
+import os
 import requests
 from dotenv import load_dotenv
 from altus_supabase.client import upsert_records
