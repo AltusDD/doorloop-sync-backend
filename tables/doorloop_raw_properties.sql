ff94138e6ac4ca6f4d162ad442d05a067a3fe5d3
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-07-19 08:47:28 -0500

    Table Make

---
+++ tables/doorloop_raw_properties.sql
@@ -0,0 +1,6 @@
+create table if not exists public.doorloop_raw_properties (
+  id text primary key,
+  data jsonb,
+  created_at timestamp with time zone default current_timestamp,
+  updated_at timestamp with time zone default current_timestamp
+);
