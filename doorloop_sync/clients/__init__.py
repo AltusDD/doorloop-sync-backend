0473019e05a2ae2ff178392466d320d87323b9b6
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-08-03 17:19:06 -0500

    Update __init__.py

---
+++ doorloop_sync/clients/__init__.py
@@ -0,0 +1,7 @@
+"""
+This file makes the client classes available for direct import
+from the 'doorloop_sync.clients' package, simplifying access
+from other parts of the application.
+"""
+from .doorloop_client import DoorLoopClient
+from .supabase_client import SupabaseClient
