4217a70d1bdeed8b80cc850360bd5b1f2e8d8f47
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-08-03 10:50:41 -0500

    tasks

--- doorloop_sync/tasks/normalization/normalize_accounts.py
+++ doorloop_sync/tasks/normalization/normalize_accounts.py
@@ -1,7 +1,7 @@
 from doorloop_sync.clients.supabase_client import SupabaseClient
 
 from doorloop_sync.utils.transform_helpers import hash_dict, clean_dict
-from doorloop_sync.clients.config import config
+from doorloop_sync.config import config
 import logging
 
 def run():
