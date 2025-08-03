a77c030b37fbfe5958e40d23b63e2b9072c8222a
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-08-03 11:24:54 -0500

    Update normalize_applications.py

--- doorloop_sync/tasks/normalization/normalize_applications.py
+++ doorloop_sync/tasks/normalization/normalize_applications.py
@@ -1,7 +1,7 @@
 from doorloop_sync.clients.supabase_client import SupabaseClient
 
 from doorloop_sync.utils.transform_helpers import hash_dict, clean_dict
-from doorloop_sync.config import config
+from doorloop_sync.config import get_supabase_client, get_logger
 import logging
 
 def run():
