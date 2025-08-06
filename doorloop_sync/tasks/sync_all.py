efe56fecefecbdc6114b4df1a51e65cc6577dbe3
AltusDD <132203980+AltusDD@users.noreply.github.com>
2025-08-06 09:43:09 -0500

    Update sync_all.py

--- doorloop_sync/tasks/sync_all.py
+++ doorloop_sync/tasks/sync_all.py
@@ -1,8 +1,24 @@
-import logging
-from doorloop_sync.tasks.raw_sync.sync_accounts import sync_accounts
-
-logger = logging.getLogger(__name__)
+from doorloop_sync.tasks.sync_properties import sync_properties
+from doorloop_sync.tasks.sync_units import sync_units
+from doorloop_sync.tasks.sync_owners import sync_owners
+from doorloop_sync.tasks.sync_tenants import sync_tenants
+from doorloop_sync.tasks.sync_leases import sync_leases
+from doorloop_sync.tasks.sync_lease_payments import sync_lease_payments
+from doorloop_sync.tasks.sync_work_orders import sync_work_orders
+from doorloop_sync.tasks.sync_vendors import sync_vendors
+from doorloop_sync.tasks.sync_files import sync_files
+from doorloop_sync.tasks.sync_notes import sync_notes
+from doorloop_sync.tasks.sync_tasks import sync_tasks
 
 def sync_all():
-    logger.info("🔁 Starting full sync process...")
-    sync_accounts()
+    sync_properties()
+    sync_units()
+    sync_owners()
+    sync_tenants()
+    sync_leases()
+    sync_lease_payments()
+    sync_work_orders()
+    sync_vendors()
+    sync_files()
+    sync_notes()
+    sync_tasks()
